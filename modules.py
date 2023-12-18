42a18ef48c4db027f3b081aac5906582a1dc2d9b
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-12-18 14:14:34 +0900

    Update modules.py
    Dialted -> Dilated

--- modules.py
+++ modules.py
@@ -83,7 +83,7 @@ class ConvReluNorm(nn.Module):
 
 class DDSConv(nn.Module):
     """
-    Dialted and Depth-Separable Convolution
+    Dilated and Depth-Separable Convolution
     """
 
     def __init__(self, channels, kernel_size, n_layers, p_dropout=0.0):
